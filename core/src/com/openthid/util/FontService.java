4cf3cce44470f59e902424005019e8ab91e24d07
Scott Ramsay <scottramsay64@gmail.com>
2015-12-15 16:10:30 +1030

    Cleans and documents FunctionalUtils + FontService. Adds `applyMany`
    Adds `FunctionalUtils.applyMany` and
    `FunctionalUtils.applyManyZip`

--- core/src/com/openthid/util/FontService.java
+++ core/src/com/openthid/util/FontService.java
@@ -11,25 +11,32 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFont
  * A lazy font system
  */
 public class FontService {
+	/**
+	 * The Ubuntu Medium font
+	 */
 	public static final FontService ubuntuMedium = new FontService("ubuntu-font-family/Ubuntu-M.ttf", "ubuntuMedium");
 
 	private String name;
 	private FreeTypeFontGenerator generator;
 	private HashMap<Integer, BitmapFont> cache;
 
-	public FontService(String filename, String name) {
+	private FontService(String filename, String name) {
 		generator = new FreeTypeFontGenerator(Gdx.files.internal(filename));
 		this.name = name;
 		cache = new HashMap<>();
 		Gdx.app.log("LAZY", "Loaded new font '" + name + "'");
 	}
 
+	/**
+	 * Lazily generates a bitmap font
+	 * @param size The size to render the font at
+	 */
 	public BitmapFont getFont(int size) {
 		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
 		parameter.size = size;
 		if (!cache.containsKey(size)) {
 			cache.put(size, generator.generateFont(parameter));
-			Gdx.app.log("LAZY", "Generating font '" + name + "' at size " + size);
+			Gdx.app.log("LAZY", "Generated font '" + name + "' at size " + size);
 		}
 		return cache.get(size);
 	}
--- core/src/com/openthid/util/FunctionalUtils.java
+++ core/src/com/openthid/util/FunctionalUtils.java
@@ -8,7 +8,13 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 
+/**
+ * A class with utility functions to assist functional programming
+ */
 public class FunctionalUtils {
+	/**
+	 * Creates a {@link ChangeListener} from a function
+	 */
 	public static ChangeListener makeChangeListener(BiConsumer<ChangeEvent, Actor> biConsumer) {
 		return new ChangeListener() {
 			@Override
@@ -18,11 +24,38 @@ public class FunctionalUtils {
 		};
 	}
 
+	/**
+	 * Takes two arrays and creates a third by applying each element from the two arrays to the given function
+	 * Same as Haskell's zipWith function.
+	 */
 	public static <A, B, C> C[] zipWith(A[] as, B[] bs, BiFunction<A, B, C> function, IntFunction<C[]> newArrayFunction) {
 		C[] cs = newArrayFunction.apply(as.length);
 		for (int i = 0; i < bs.length; i++) {
 			cs[i] = function.apply(as[i], bs[i]);
 		}
 		return cs;
+	}
+
+	public static <A, B> A applyMany(A a, B[] bs, BiConsumer<A, B> biConsumer) {
+		for (int i = 0; i < bs.length; i++) {
+			biConsumer.accept(a, bs[i]);
+		}
+		return a;
+	}
+
+	/**
+	 * 
+	 * @param a
+	 * @param bs
+	 * @param cs
+	 * @param biConsumer
+	 * @return a
+	 * throws {@link ArrayIndexOutOfBoundsException} If cs.length < bs.length
+	 */
+	public static <A, B, C> A applyManyZip(A a, B[] bs, C[] cs, TriConsumer<A, B, C> triConsumer) {
+		for (int i = 0; i < bs.length; i++) {
+			triConsumer.accept(a, bs[i], cs[i]);
+		}
+		return a;
 	}
 }
