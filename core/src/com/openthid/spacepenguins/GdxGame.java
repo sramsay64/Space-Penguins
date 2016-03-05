08c63d61ecfac6adeadb6e84a942d47602ef1b8b
Scott Ramsay <scottramsay64@gmail.com>
2016-03-05 17:54:04 +1030

    Removes badlogic logo texture

--- core/src/com/openthid/spacepenguins/GdxGame.java
+++ core/src/com/openthid/spacepenguins/GdxGame.java
@@ -1,7 +1,6 @@
 package com.openthid.spacepenguins;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kotcrab.vis.ui.VisUI;
 import com.openthid.spacepenguins.field.FieldScreen;
@@ -10,7 +9,6 @@ import com.openthid.spacepenguins.screens.TitleScreen;
 
 public class GdxGame extends Game {
 	private SpriteBatch batch;
-	private Texture img;
 
 	private int width;
 	private int height;
@@ -26,17 +24,12 @@ public class GdxGame extends Game {
 
 	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 		VisUI.load();
 		setScreen(getTitleScreen());
 	}
 
 	public SpriteBatch getBatch() {
 		return batch;
-	}
-
-	public Texture getImg() {
-		return img;
 	}
 
 	public int getHeight() {
